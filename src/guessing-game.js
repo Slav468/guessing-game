b695abcd7a76712b2c96c27b9a5613bf739c7f62
Slav468 <slav1988@tut.by>
2023-02-09 12:29:38 +0300

    complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,23 @@
 class GuessingGame {
-    constructor() {}
+	constructor() {}
 
-    setRange(min, max) {
+	setRange(min, max) {
+		this.min = min;
+		this.max = max;
+	}
 
-    }
+	guess() {
+		this.number = Math.round((this.min + this.max) / 2);
+		return this.number;
+	}
 
-    guess() {
+	lower() {
+		this.max = this.number;
+	}
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+	greater() {
+		this.min = this.number;
+	}
 }
 
 module.exports = GuessingGame;
